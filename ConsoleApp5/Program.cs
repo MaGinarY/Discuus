cd372869862108256865ff4c520637fef1904670
MaGinarY <MaGinarY@DESKTOP-A7QFV3Q>
2023-03-22 19:46:08 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter a word: ");
+                string word = Console.ReadLine().ToLower();
+
+                int[] count = new int[2];
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsLetter(word[i]))
+                    {
+                        int index = word[i] - 'a';
+                        count[index]++;
+                    }
+                }
+
+                for (int i = 0; i < count.Length; i++)
+                {
+                    if (count[i] != 0)
+                    {
+                        Console.WriteLine((char)(i + 'a') + ": " + count[i]);
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+}   
+}
